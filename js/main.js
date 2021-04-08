fa8baeeacf0f063a62f09bf4a98bc91f9c7d7985
Nathan English <45088619+impactx@users.noreply.github.com>
2021-04-08 10:44:18 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const mylinks = document.getElementById('myLinks');
+
+function navHamburger() {
+    
+    if (mylinks.style.display === 'block') {
+        mylinks.style.display = 'none';
+
+    } else {
+        mylinks.style.display = 'block';
+
+    }
+
+
+
+}
+
+function reportWindowSize() {
+    let width = window.innerWidth;
+    if (width >= 992) {
+        mylinks.style.display = "none";
+    }
+}
+
+window.addEventListener('resize', reportWindowSize);
+
+
+function artifacts() {
+    const artifact = document.getElementById('artifact');
+    
+    if (artifact.style.display === 'flex') {
+        artifact.style.display = 'none';
+    } else {
+        artifact.style.display = 'flex'
+    }
+}
