d57f780248b9bff24ac81fdce3e13ba5dfb7df0d
Nathan English <45088619+impactx@users.noreply.github.com>
2021-04-17 17:58:24 -0500

    Artifact Pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const mylinks = document.getElementById('myLinks');
 const artifact = document.getElementById('artifact');
-const statement_div = document.getElementById('statement')
 
 
+function statementFunction() {
+    const short = 'Upon starting my doctoral studies, I was sure I was going to be a qualitative researcher. As part of my program of study I started taking quantitative research classes first. After completing my first class I was surprised to find that I had found a strong interest in statistics.'
+    const full_short = ` I have found through taking classes in this program that statistics are a useful and valuable tool in research.  The ability to be able to use statistics for research is something that is sought after and rewarding.  Being able to complete statistical analysis for research projects with your peers and students is a skill that I will use to help others develop their own research as well as help work with on my own.`
+    const p = document.getElementById('personalstatement');
+    const para = document.getElementById('paragraph-two');
+    const sButton = document.getElementById('statement-link');
+    // IF the 2nd paragraph is showing
+    if (para.style.display === "none") {
+        para.style.display = 'block';
+        p.innerText += full_short;
+        sButton.innerHTML = "Show Less..."
+    } else {
+        para.style.display = 'none'
+        p.innerText = short;
+        sButton.innerHTML = "Read More..."
+
+    }
+}
 
 function navHamburger() {
 
@@ -11,7 +28,6 @@ function navHamburger() {
         artifact.style.display = "none";
     } else {
         mylinks.style.display = 'block';
-
     }
 
 
